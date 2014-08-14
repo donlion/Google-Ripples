d9ccd54ebc404600c8f9e8f31a4aebf4a1ccebd1
Leo Ørsnes <zeekooster@gmail.com>
2014-08-14 15:27:22 +0200

    Added Ripples()
    with Ripples.init() and Ripples.setup()

---
+++ ripples.js
@@ -0,0 +1,48 @@
+var Ripples = new function() {
+
+  this.setup = function() {
+
+    var append = ".ripple{display:block;position:absolute;background-color:rgba(0,0,0,.033);border-radius:50%;-webkit-transform:scale(0);-moz-transform:scale(0);-ms-transform:scale(0);-o-transform:scale(0);transform:scale(0);-webkit-transition:opacity .3s ease-in;-moz-transition:opacity .3s ease-in;transition:opacity .3s ease-in;opacity:0;pointer-events:none;&.animate{opacity:1;-webkit-animation:ripple .65s ease-in;-webkit-animation-fill-mode:forwards;-moz-animation:ripple .65s ease-in;-moz-animation-fill-mode:forwards;-o-animation:ripple .65s ease-in;-o-animation-fill-mode:forwards;animation:ripple .65s ease-in;animation-fill-mode:forwards}@-webkit-keyframes ripple{75%{opacity:1}100%{opacity:0;-webkit-transform:scale(2)}}@-moz-keyframes ripple{75%{opacity:1}100%{opacity:0;-moz-transform:scale(2)}}@-o-keyframes ripple{75%{opacity:1}100%{opacity:0;-o-transform:scale(2)}}@-ms-keyframes ripple{75%{opacity:1}100%{opacity:0;-ms-transform:scale(2)}}@keyframes ripple{75%{opacity:1}100%{opacity:0;transform:scale(2)}}";
+    $("header").append("<style>"+append+"</style");
+
+  };
+
+  this.init = function()  {
+      $("[data-ripple]").each(function() {
+          if ($(this).css("overflow") != "hidden") {
+              $(this).css("overflow", "hidden");
+          }
+      });
+
+      $("[data-ripple]").click(function(e) {
+          if ($(this).find(".ripple").length != 0) {
+              $(this).find(".ripple").remove();
+          }
+          var parent = $(this).parent();
+
+          $(this).prepend("<span class='ripple' />");
+
+          var ripple = $(this).find(".ripple");
+
+          if (!ripple.height() || !ripple.width()) {
+              var d = Math.max(parent.outerWidth(), parent.outerHeight());
+              ripple.css({
+                  height: d,
+                  width: d
+              });
+          }
+
+          var x = e.pageX - $(this).offset().left - ripple.width() / 2;
+          var y = e.pageY - $(this).offset().top - ripple.height() / 2;
+
+          ripple.css({
+              top: y + 'px',
+              left: x + 'px'
+          }).addClass("animate");
+          setTimeout(function() {
+              ripple.remove();
+          }, 650);
+      });
+  };
+
+};

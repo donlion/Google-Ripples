30f7d7a71c76c32ab78aafb5b0c0cc6f0ccf390b
Leo Ørsnes <zeekooster@gmail.com>
2014-08-14 15:44:53 +0200

    Stable build + demo

--- ripples.js
+++ ripples.js
@@ -2,15 +2,18 @@ var Ripples = new function() {
 
   this.setup = function() {
 
-    var append = ".ripple{display:block;position:absolute;background-color:rgba(0,0,0,.033);border-radius:50%;-webkit-transform:scale(0);-moz-transform:scale(0);-ms-transform:scale(0);-o-transform:scale(0);transform:scale(0);-webkit-transition:opacity .3s ease-in;-moz-transition:opacity .3s ease-in;transition:opacity .3s ease-in;opacity:0;pointer-events:none;&.animate{opacity:1;-webkit-animation:ripple .65s ease-in;-webkit-animation-fill-mode:forwards;-moz-animation:ripple .65s ease-in;-moz-animation-fill-mode:forwards;-o-animation:ripple .65s ease-in;-o-animation-fill-mode:forwards;animation:ripple .65s ease-in;animation-fill-mode:forwards}@-webkit-keyframes ripple{75%{opacity:1}100%{opacity:0;-webkit-transform:scale(2)}}@-moz-keyframes ripple{75%{opacity:1}100%{opacity:0;-moz-transform:scale(2)}}@-o-keyframes ripple{75%{opacity:1}100%{opacity:0;-o-transform:scale(2)}}@-ms-keyframes ripple{75%{opacity:1}100%{opacity:0;-ms-transform:scale(2)}}@keyframes ripple{75%{opacity:1}100%{opacity:0;transform:scale(2)}}";
-    $("header").append("<style>"+append+"</style");
+    var append = ".ripple{display:block;position:absolute;background-color:rgba(0,0,0,.033);border-radius:50%;-webkit-transform:scale(0);-moz-transform:scale(0);-ms-transform:scale(0);-o-transform:scale(0);transform:scale(0);-webkit-transition:opacity .3s ease-in;-moz-transition:opacity .3s ease-in;transition:opacity .3s ease-in;opacity:0;pointer-events:none}.ripple.animate{opacity:1;-webkit-animation:ripple .65s ease-in;-webkit-animation-fill-mode:forwards;-moz-animation:ripple .65s ease-in;-moz-animation-fill-mode:forwards;-o-animation:ripple .65s ease-in;-o-animation-fill-mode:forwards;animation:ripple .65s ease-in;animation-fill-mode:forwards}@-webkit-keyframes ripple{75%{opacity:1}100%{opacity:0;-webkit-transform:scale(2)}}@-moz-keyframes ripple{75%{opacity:1}100%{opacity:0;-moz-transform:scale(2)}}@-o-keyframes ripple{75%{opacity:1}100%{opacity:0;-o-transform:scale(2)}}@-ms-keyframes ripple{75%{opacity:1}100%{opacity:0;-ms-transform:scale(2)}}@keyframes ripple{75%{opacity:1}100%{opacity:0;transform:scale(2)}}";
+    $("head").append("<style>"+append+"</style");
 
   };
 
   this.init = function()  {
       $("[data-ripple]").each(function() {
           if ($(this).css("overflow") != "hidden") {
-              $(this).css("overflow", "hidden");
+            $(this).css("overflow", "hidden");
+          }
+          if ($(this).css("position") != "relative") {
+            $(this).css("position", "relative");
           }
       });
 
@@ -18,11 +21,14 @@ var Ripples = new function() {
           if ($(this).find(".ripple").length != 0) {
               $(this).find(".ripple").remove();
           }
-          var parent = $(this).parent();
 
           $(this).prepend("<span class='ripple' />");
 
           var ripple = $(this).find(".ripple");
+
+          var parent = ripple.parent();
+
+          console.log(ripple.height()+", "+ripple.width());
 
           if (!ripple.height() || !ripple.width()) {
               var d = Math.max(parent.outerWidth(), parent.outerHeight());
@@ -32,8 +38,11 @@ var Ripples = new function() {
               });
           }
 
+          console.log(ripple.height()+", "+ripple.width());
+
           var x = e.pageX - $(this).offset().left - ripple.width() / 2;
           var y = e.pageY - $(this).offset().top - ripple.height() / 2;
+
 
           ripple.css({
               top: y + 'px',
